13f68fdef10329300aff7d91a20ec92d74a24ed6
agakhil13 <aguptaji1310@gmail.com>
2022-04-16 16:34:22 +0530

    makemigrations, sqlmigrate Appname migrate name, migrate

--- Homepage/models.py
+++ Homepage/models.py
@@ -1,35 +1,31 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
 
-class Banner:
-    id : int
-    faded_text : str
-    text : str
-    bold_text : str
-    img : str
+class Banner(models.Model):
+    faded_text = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    bold_text = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='Banner')
 
-class Box_Category:
-    id: int
-    title : str
-    img : str
+class Box_Category(models.Model):
+    title = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='Boxes')
 
-class Hot_Tour:
-    id : int
-    title : str
-    desc : str
-    review : int
-    img : str
-    price : int
-    rating: list
+class Hot_Tour(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.TextField()
+    review = models.IntegerField()
+    img = models.ImageField(upload_to='Hot_Tour')
+    price = models.IntegerField()
+    rating= models.IntegerField()
 
-class Teams:
-    id : int
-    name : str
-    phone : str
-    status : str
-    img : str
+class Teams(models.Model):
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    status = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='Team')
 
-class Wonder:
-    id : int
-    img : str
+class Wonder(models.Model):
+    img = models.ImageField(upload_to='Wonder')
