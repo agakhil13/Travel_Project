d0e6c2537e0287ade8f7ebeeb395e6dee55d3c48
agakhil13 <aguptaji1310@gmail.com>
2022-04-16 16:42:16 +0530

    register model in admin.py

--- Homepage/admin.py
+++ Homepage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Banner, Box_Category, Hot_Tour, Teams, Wonder
 # Register your models here.
+admin.site.register(Banner)
+admin.site.register(Box_Category)
+admin.site.register(Hot_Tour)
+admin.site.register(Teams)
+admin.site.register(Wonder)
