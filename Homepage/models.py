70cc9aec805db3419dc818b0acb770a8aee6e8a0
agakhil13 <aguptaji1310@gmail.com>
2022-04-16 10:53:04 +0530

    created model for banner

--- Homepage/models.py
+++ Homepage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Banner:
+    id : int
+    faded_text : str
+    text : str
+    bold_text : str
+    img : str
+
--- Homepage/views.py
+++ Homepage/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from .models import Banner
 
 # Create your views here.
+def banner():
+    ban1 = Banner()
+    ban1.faded_text = "Enjoy the Best Destinations with Our Travel Agency"
+    ban1.text = "Explore"
+    ban1.bold_text = "The World"
+    ban1.img = "slider-4-slide-1-1920x678.jpg"
+
+    ban2 = Banner()
+    ban2.faded_text = "A team of professional Travel Experts"
+    ban2.text = "Trust"
+    ban2.bold_text = "Our Experience"
+    ban2.img = "slider-4-slide-2-1920x678.jpg"
+
+    ban3 = Banner()
+    ban3.faded_text = "Build your Next Holiday Trip with Us"
+    ban3.text = "Create"
+    ban3.bold_text = "Your Tour"
+    ban3.img = "slider-4-slide-3-1920x678.jpg"
+
+    ban = [ban1, ban2, ban3]
+    return ban
+
 def page(request):
-    return render(request,'index.html')
+    return render(request,'index.html', {'banner': banner()})
 
 def about(request):
     return render(request,'about.html')
